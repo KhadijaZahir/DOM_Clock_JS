8f485732e00d8764691c8b09e9d81144c4c8039f
KhadijaZahir <57948334+KhadijaZahir@users.noreply.github.com>
2020-04-12 22:02:45 +0100

    finished clock by javascript

--- script.js
+++ script.js
@@ -5,20 +5,34 @@ const AIGUILLESEC = document.querySelector("#second");
 
 //Extraire l'heure actuel à l'aide de l'objet Date()
 
+// var today = new Date();
 
 //Ajouter l'heure , minite , seconde  dans des varaiables
 
-
+// var hour = today.getHours();
+//  var min = today.getMinutes();
+//  var sec = today.getSeconds();
 
 // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
 // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
 
-
+// var hourhand = hour*360/12 + ((min * 360/60)/12);
+// var minhand= (min * 360/60) + (sec* 360/60)/60;
+// var sechand = sec * 360/60;
 // Déplacer les aiguilles 
 function demarrerLaMontre() {
+  var today = new Date();
+  var hour = today.getHours();
+   var min = today.getMinutes();
+   var sec = today.getSeconds();
 
+   var hourDeg = hour*360/12 + ((min * 360/60)/12);
+   var minDeg= (min * 360/60) + (sec* 360/60)/60;
+   var secDeg = sec * 360/60;
 
-
+   AIGUILLEHR.style.transform = `rotate(${hourDeg}deg)`;
+   AIGUILLEMIN.style.transform = `rotate(${minDeg}deg)`;
+   AIGUILLESEC.style.transform = `rotate(${secDeg}deg)`;
 }
 // Exercuter la fonction chaque second
 var interval = setInterval(demarrerLaMontre, 1000);
